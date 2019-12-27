89d60fc26f9ace9eaa0e48e1b3374d48913e5efd
Dejan <dejanlaco@gmail.com>
2019-12-27 17:42:30 +0100

    front-end - add new route (all authors) to vue, add template component - list all authors

---
+++ resources/js/components/AllAuthors.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h3>All Authors</h3>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Address</th>
+                    <th>Books</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="author in authors" :key="author.id">
+                    <td>{{ author.id }}</td>
+                    <td>{{ author.name }}</td>
+                    <td>{{ author.age }}</td>
+                    <td>{{ author.address }}</td>
+                    <td>0</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                authors: []
+            }
+        },
+        created() {
+            this.axios
+                .get('/api/authors')
+                .then(response => {
+                    this.authors = response.data;
+                });
+        }
+    }
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,15 @@
 import AllBooks from './components/AllBooks.vue';
+import AllAuthors from './components/AllAuthors.vue';
 
 export const routes = [
     {
         name: 'home',
         path: '/',
         component: AllBooks
+    },
+    {
+        name: 'authors',
+        path: '/authors',
+        component: AllAuthors
     }
 ];
